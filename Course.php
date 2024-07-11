ae3b4e78dd44b7537b52603264525f25bc74e572
markeze <85074748+markeze207@users.noreply.github.com>
2024-07-11 13:16:10 +0300

    Add files via upload

--- Course.php
+++ Course.php
@@ -1,9 +1,11 @@
 <?php
 
-abstract class Courses extends RestApi
+class Course extends Courses
 {
 
     protected $modxClass;
+
+    protected $courseId;
 
     protected $api;
 
@@ -11,16 +13,17 @@ abstract class Courses extends RestApi
 
     protected $course;
 
-    public function __construct(modX $modxClass, $apiKey) {
+    public function __construct(modX $modxClass, $courseId, $apiKey) {
         $this->modxClass = $modxClass;
+        $this->courseId = $courseId;
 
         $this->api = new RestApi($modxClass, $apiKey);
     }
 
     // Получаем курс по айдишнику
-    public function getCourseById($courseId)
+    public function getCourse()
     {
-        $result = $this->api->get('courses.get', ['ID' => $courseId]);
+        $result = $this->api->get('courses.get', ['ID' => $this->courseId]);
         if(!empty($result['result']['item']))
         {
             $this->course = $result['result']['item'][0];
@@ -30,9 +33,9 @@ abstract class Courses extends RestApi
     }
 
     // Проверяем, курс запущен или нет
-    public function isStartById($group_id, $courseId)
+    public function isStart($group_id)
     {
-        $resultGet = $this->api->get('courses_start.get', ['group_id' => $group_id, 'course_id' => $courseId]);
+        $resultGet = $this->api->get('courses_start.get', ['group_id' => $group_id, 'course_id' => $this->courseId]);
         if(!empty($resultGet['result']['item']))
         {
             return $resultGet['result']['item'][0]['ID'];
@@ -42,9 +45,9 @@ abstract class Courses extends RestApi
     }
 
     // Запускаем курс
-    public function startCourseById($group_id, $courseId): bool
+    public function startCourse($group_id): bool
     {
-        $result = $this->api->get('courses_start.add', ['group_id' => $group_id, 'course_id' => $courseId]);
+        $result = $this->api->get('courses_start.add', ['group_id' => $group_id, 'course_id' => $this->courseId]);
         if($result)
         {
             return true;
@@ -54,9 +57,9 @@ abstract class Courses extends RestApi
     }
 
     // Заканчиваем курс
-    public function endCourseById($group_id, $courseId): bool
+    public function endCourse($group_id): bool
     {
-        $resultStart = $this->isStart($group_id, $courseId);
+        $resultStart = $this->isStart($group_id);
         if($resultStart)
         {
             $result = $this->api->get('courses_start.delete', ['id' => $resultStart]);
@@ -72,13 +75,12 @@ abstract class Courses extends RestApi
     }
 
     // Получаем даты курса
-    public function getDatesById($courseId)
+    public function getDates()
     {
-        $course = $this->getCourseById($courseId);
-        if($course->course)
+        if($this->course)
         {
-            $date_course = $course['PROPERTY_1254'];
-
+            $date_course = $this->course['PROPERTY_1254'];
+            
             $dates_arr = json_decode($date_course, true);
             $dates_out = [];
             $idx_time = 0;
@@ -96,10 +98,26 @@ abstract class Courses extends RestApi
         return false;
     }
 
-    // Получаем время начала и конца курса
-    public function getTimeCourseById($group_id, $courseId)
+    // Получаем все даты от даты начала до даты конца
+    public function getFilterDate($date_start, $date_end)
     {
-        $res = $this->api->get('time_course.get', ['group_id' => $group_id, 'course_id' => $courseId]);
+        $start_date = date_create($date_start);
+        $end_date   = date_create(date('Y-m-d', strtotime($date_end . "+1 days")));
+        $interval = DateInterval::createFromDateString('1 day');
+        $daterange = new DatePeriod($start_date, $interval, $end_date);
+
+        $dates_out_interval_get = [];
+
+        foreach($daterange as $dr){
+            $dates_out_interval_get[] = $dr->format('Y-m-d');
+        }
+        return $dates_out_interval_get;
+    }
+
+    // Получаем время начала и конца курса
+    public function getTimeCourse($group_id)
+    {
+        $res = $this->api->get('time_course.get', ['group_id' => $group_id, 'course_id' => $this->courseId]);
         if(!empty($res['result']['item']))
         {
             return $res['result']['item'][0];
@@ -107,11 +125,23 @@ abstract class Courses extends RestApi
         return false;
     }
 
+    // Получаем/устанавливаем group_id
+    public function setGroupId($date_end): string
+    {
+        $dateTime = new DateTime($date_end);
+
+        // Форматируем дату в нужном виде
+        $formattedDate = $dateTime->format("d.m.Y");
+
+        $this->groupId = $formattedDate.'-'.$_POST['course_id'];
+
+        return $this->groupId;
+    }
 
     // Устанавливаем время для курса
-    public function setTimeCourseById($group_id, $time_start, $time_end, $courseId)
+    public function setTimeCourse($group_id, $time_start, $time_end)
     {
-        $res = $this->api->get('time_course.get', ['group_id' => $group_id, 'course_id' => $courseId]);
+        $res = $this->api->get('time_course.get', ['group_id' => $group_id, 'course_id' => $this->courseId]);
 
         $result = false;
 
@@ -119,7 +149,7 @@ abstract class Courses extends RestApi
         {
             $result = $this->api->get('time_course.update', ['ID' => $res['result']['item'][0]['ID'], 'time_end' => $time_end, 'time_start' => $time_start]);
         } else {
-            $result = $this->api->get('time_course.add', ['group_id' => $group_id, 'course_id' => $courseId, 'time_end' => $time_end, 'time_start' => $time_start]);
+            $result = $this->api->get('time_course.add', ['group_id' => $group_id, 'course_id' => $this->courseId, 'time_end' => $time_end, 'time_start' => $time_start]);
         }
         return $result;
     }
